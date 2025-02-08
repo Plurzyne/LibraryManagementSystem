2b322ada8d88f9c120da74361dc16cb463be889a
Plurzyne <ahmedovnurlan00@gmail.com>
2025-02-08 13:01:00 +0400

    added comments

--- Book/Book.java
+++ Book/Book.java
@@ -1,17 +1,23 @@
 package Book;
 
 class Book{
+    // Properties of a book
     String title;
     String author;
     int pages;
     boolean availability;
 
+    //Initializing the properties of a book
+    
     public Book(String title, String author, int pages,boolean availability){
         this.title=title;
         this.author=author;
         this.pages=pages;
         this.availability=availability;
     }
+
+    // Displaying a details of a book
+    
     public static void main(String[] args) {
         Book b=new Book("Java","James Gosling",500,true);
         System.out.println("Title: "+b.title);
