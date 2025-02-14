b2c3cb6f46709af2ccdf000c95d8605c5cdf4ae1
Plurzyne <ahmedovnurlan00@gmail.com>
2025-02-14 14:51:20 +0400

    added property Price

--- Book/Book.java
+++ Book/Book.java
@@ -1,28 +1,38 @@
 package Book;
 
-class Book{
+import java.util.*;
+
+class Book {
     // Properties of a book
     String title;
     String author;
-    int pages;
+    long pages;
     boolean availability;
+    double price;
 
-    //Initializing the properties of a book
-    
-    public Book(String title, String author, int pages,boolean availability){
-        this.title=title;
-        this.author=author;
-        this.pages=pages;
-        this.availability=availability;
+
+    // Initializing the properties of a book
+    public Book(String title,
+                String author,
+                long pages,
+                double price,
+                boolean availability) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.price = price;
+        this.availability = availability;
     }
 
     // Displaying a details of a book
-    
+
     public static void main(String[] args) {
-        Book b=new Book("Java","James Gosling",500,true);
-        System.out.println("Title: "+b.title);
-        System.out.println("Author: "+b.author);
-        System.out.println("Pages: "+b.pages);
-        System.out.println("Availability: "+b.availability);
+        Book b = new Book("Java", "James Gosling", 248, 39.99, true);
+        System.out.println("Title: " + b.title);
+        System.out.println("Author: " + b.author);
+        System.out.println("Pages: " + b.pages);
+        System.out.println("Price: $" + b.price);
+        System.out.println("Availability: " + b.availability);
+
     }
 }
